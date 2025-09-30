a8cb479615a577a904488e0e7520995fe72fca12
Nikola  Bogdanovic <bogdanik@students.zhaw.ch>
2025-09-30 15:54:49 +0200

    Implemented get company by id endpoint. Closes #4.

--- src/main/java/ch/zhaw/freelancer4u/controller/CompanyController.java
+++ src/main/java/ch/zhaw/freelancer4u/controller/CompanyController.java
@@ -1,37 +1,48 @@
-package ch.zhaw.freelancer4u.controller; 
- 
+package ch.zhaw.freelancer4u.controller;
+
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired; 
-import org.springframework.http.HttpStatus; 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping; 
-import org.springframework.web.bind.annotation.RequestBody; 
-import org.springframework.web.bind.annotation.RestController; 
-import org.springframework.web.bind.annotation.RequestMapping; 
- 
-import ch.zhaw.freelancer4u.model.Company; 
-import ch.zhaw.freelancer4u.model.CompanyCreateDTO; 
-import ch.zhaw.freelancer4u.repository.CompanyRepository; 
- 
-@RestController 
-@RequestMapping("/api") 
-public class CompanyController { 
-    @Autowired 
-    CompanyRepository companyRepository; 
- 
-    @PostMapping("/company") 
-    public ResponseEntity<Company> createCompany( 
-        @RequestBody CompanyCreateDTO fDTO) { 
-        Company fDAO = new Company(fDTO.getName(), fDTO.getEmail()); 
-        Company f = companyRepository.save(fDAO); 
-        return new ResponseEntity<>(f, HttpStatus.CREATED); 
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import ch.zhaw.freelancer4u.model.Company;
+import ch.zhaw.freelancer4u.model.CompanyCreateDTO;
+import ch.zhaw.freelancer4u.repository.CompanyRepository;
+
+@RestController
+@RequestMapping("/api")
+public class CompanyController {
+    @Autowired
+    CompanyRepository companyRepository;
+
+    @PostMapping("/company")
+    public ResponseEntity<Company> createCompany(
+            @RequestBody CompanyCreateDTO fDTO) {
+        Company fDAO = new Company(fDTO.getName(), fDTO.getEmail());
+        Company f = companyRepository.save(fDAO);
+        return new ResponseEntity<>(f, HttpStatus.CREATED);
     }
-    
-     @GetMapping("/company")
-        public ResponseEntity<List<Company>> getAllCompanies() {
+
+    @GetMapping("/company")
+    public ResponseEntity<List<Company>> getAllCompanies() {
         List<Company> companies = companyRepository.findAll();
         return ResponseEntity.ok(companies);
     }
-} 
+
+    @GetMapping("/company/{id}")
+    public ResponseEntity<Company> getCompanyById(@PathVariable String id) {
+        Optional<Company> result = companyRepository.findById(id);
+        if (result.isPresent()) {
+            return new ResponseEntity<>(result.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+}
