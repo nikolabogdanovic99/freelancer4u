b1246f89076930163fc1d8a091b884bc4397098b
Nikola  Bogdanovic <bogdanik@students.zhaw.ch>
2025-10-27 22:55:49 +0100

    added login to frontend

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,13 +1,13 @@
 <script>
   import favicon from "$lib/assets/favicon.svg";
   import "./styles.css";
-  let { children } = $props();
+  let { data, children } = $props();
+  let { user, isAuthenticated } = data;
 </script>
 
 <svelte:head>
   <link rel="icon" href={favicon} />
 </svelte:head>
-
 <nav class="navbar navbar-expand-lg bg-light">
   <div class="container-fluid">
     <a class="navbar-brand" href="/">Freelancer4U</a>
@@ -23,19 +23,34 @@
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link" aria-current="page" href="/companies">Companies</a
-          >
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/jobs">Jobs</a>
-        </li>
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        {#if isAuthenticated}
+          <li class="nav-item">
+            <a class="nav-link" aria-current="page" href="/companies"
+              >Companies</a
+            >
+          </li>
+        {/if}
+        {#if isAuthenticated}
+          <li class="nav-item">
+            <a class="nav-link" href="/jobs">Jobs</a>
+          </li>
+        {/if}
       </ul>
+      <div class="d-flex">
+        {#if isAuthenticated}
+          <span class="navbar-text me-2">{user.name}</span>
+          <form method="POST" action="/logout" style="display: inline;">
+            <button type="submit" class="btn btn-primary">Log Out</button>
+          </form>
+        {:else}
+          <a href="/login" class="btn btn-primary me-2">Login</a>
+          <a href="/signup" class="btn btn-outline-primary">Sign Up</a>
+        {/if}
+      </div>
     </div>
   </div>
 </nav>
-
 <div class="container mt-3">
   {@render children()}
 </div>
